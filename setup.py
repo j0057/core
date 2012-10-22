742049f5fb42f12863921dbfbfd9cee0d384e91d
Joost Molenaar <j.j.molenaar@gmail.com>
2012-10-22 06:44:12 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2.7
+
+from distutils.core import setup
+
+setup(name="www-core"
+    , author="Joost Molenaar"
+    , author_email="j.j.molenaar@gmail.com"
+    , package_dir={ "": "py" }
+    , packages=["core"]
+    , version="0.1.0"
+    )
+
