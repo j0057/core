eb41d4a58e4d05ed5c069db3959693b583fbd434
Joost Molenaar <j.j.molenaar@gmail.com>
2012-10-29 11:18:20 +0100

    Switching to multi-package setup

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python2.7
-
 from distutils.core import setup
 
-setup(name="www-core"
-    , author="Joost Molenaar"
-    , author_email="j.j.molenaar@gmail.com"
-    , package_dir={ "": "py" }
-    , packages=["core"]
-    , version="0.1.0"
-    )
+setup(
+    name="j0057nl.core",
+    packages=["j0057nl.core"],
+    version="0.1.0",
+    author="Joost Molenaar",
+    author_email="j.j.molenaar@gmail.com")
 
