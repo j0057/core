0a8eb4629710cc515239d14d1d353bdf9aa02ef4
Joost Molenaar <j.j.molenaar@gmail.com>
2013-08-03 08:32:46 +0200

    Rename jjm.core to core

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(
     author="Joost Molenaar",
     author_email="j.j.molenaar@gmail.com",
-    name="jjm.core",
-    packages=["jjm.core"],
-    url="https://github.com/JoostMolenaar/jjm.core",
+    name="core",
+    packages=["core"],
+    url="https://github.com/j0057/core",
     version="0.1.0",
 )
 
